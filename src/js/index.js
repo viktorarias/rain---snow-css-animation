cb9955f2ce554361f195bff5c1abb9da38be7994
Víctor Arias <vik@Victors-MacBook-Pro.local>
2019-02-28 23:22:39 -0500

    CSS Rain and Snow development example

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import {Tweenmax} from 'gsap/TweenMax';
 
 
-const numberOfDrops = 150;
+const numberOfDrops = 300;
 const rainContainer = document.querySelector('#rain-container');
 for(let i = 0; i < numberOfDrops; i++){
 
@@ -11,29 +11,24 @@ for(let i = 0; i < numberOfDrops; i++){
  rainContainer.appendChild(createDrop);
 
 }
- const drop = document.querySelectorAll('drop');
+ const drop = document.querySelectorAll('.drop');
+ const buttonRain = document.querySelector('#rain'); 
+ const buttonSnow = document.querySelector('#rain'); 
 
-const timeLine = new TimelineMax();
-timeLine
-.addLabel('frame1')
-.to(drop, 0, {css:{className:'+=rain'}}, 'frame1')
+ function rain(){
+    drop.forEach((element) => {
+        element.classList.remove('snow');
+        element.classList.add('rain');
+    }
+    )
+ };
+ function snow(){
+    drop.forEach((element) => {
+        element.classList.remove('rain');
+        element.classList.add('snow');
+    }
+    )
+ };
 
-.addLabel('frame2','+=.2')
-.to(drop, 0, {css:{className:'+=snow'}}, 'frame2')
-// .to([creative.dom.f2Snow1, creative.dom.f2Snow2,creative.dom.bgExit],0,{zIndex: 1});
-
-function removeRain() {
-var arr = Array.prototype.slice.call(creative.dom.drop);
-
-arr.forEach(function(el) {
-el.classList.remove('rain');
-});
-
-}
-function removeSnow() {
-var arr = Array.prototype.slice.call(creative.dom.drop);
-
-arr.forEach(function(el) {
-el.classList.remove('snow');
-});
-}
+ buttonRain.addEventListener('click', rain);
+ buttonSnow.addEventListener('click', snow);
